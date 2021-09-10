8576c6790565e71c2b18eb6401de4d10bc02b2c5
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-10 09:52:19 +0800

    Create relational.java

---
+++ relational.java
@@ -0,0 +1,23 @@
+
+public class relational {
+
+	public static void main(String[] args) {
+		
+		// create variables
+		int a = 7, b = 11;
+		// == operator
+		System.out.println(a == b); // false
+		// != operator
+		System.out.println(a != b); // true
+		// > operator
+		System.out.println(a > b); // false
+		// < operator
+		System.out.println(a < b); // true
+		// >= operator
+		System.out.println(a >= b); // false
+		// <= operator
+		System.out.println(a <= b); // true
+	   
+	}
+
+}
