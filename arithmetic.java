df8ddffe7312b458b06fd07b7b324b28a135c858
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-10 09:30:20 +0800

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,19 @@
+public class arithmetic {
+ 
+ public static void main(String[] args) {  
+ 
+  int num1 = 5;
+  int num2 = 10;
+ 
+  System.out.println(num1 + num2);
+   
+  System.out.println(num1 - num2);
+   
+  System.out.println(num1 * num2);
+   
+  System.out.println(num1 / num2);
+ 
+  System.out.println(num1 % num2);
+ }
+ 
+}
