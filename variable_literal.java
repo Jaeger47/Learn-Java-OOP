27f850318c9eb622eacd1b24a86cb7390b6b0d24
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-10 09:48:59 +0800

    Create variable_literal.java

---
+++ variable_literal.java
@@ -0,0 +1,30 @@
+
+public class variable_literal {
+
+	public static void main(String[] args) {
+		
+		//Java Primitive Data Types
+		Boolean bool = true; //True or False
+	    byte num1 = 127; //-128 to 127
+	    short num2 = 500; //-32768 to 32767
+	    int num3 = 9999; //-2,147,483,648 to 2,147,483,647
+	    long num4 = 555; //-2^63 to 2^63-1
+	    double num5 = 47.001; //Double-precision 64-bit floating-point
+	    float num6 = 100.23f; //single precision 32-bit floating-point
+	    char charac = 'Z'; //16-bit Unicode
+	    String word = "Erika"; //array of characters
+	    
+	    //displaying the output
+	    System.out.println(bool);
+	    System.out.println(num1);
+	    System.out.println(num2);
+	    System.out.println(num3);
+	    System.out.println(num4);
+	    System.out.println(num5);
+	    System.out.println(num6);
+	    System.out.println(charac);
+	    System.out.println(word);
+	   
+	}
+
+}
