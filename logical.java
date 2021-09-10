17557cecb827de8963f993f347d3a737cfa64e73
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-10 09:55:07 +0800

    Create logical.java

---
+++ logical.java
@@ -0,0 +1,19 @@
+
+public class logical {
+
+	public static void main(String[] args) {
+		
+		// && operator
+		System.out.println((5 > 3) && (8 > 5)); // true
+		System.out.println((5 > 3) && (8 < 5)); // false
+		// || operator
+		System.out.println((5 < 3) || (8 > 5)); // true
+		System.out.println((5 > 3) || (8 < 5)); // true
+		System.out.println((5 < 3) || (8 < 5)); // false
+		// ! operator
+		System.out.println(!(5 == 3)); // true
+		System.out.println(!(5 > 3)); // false
+	   
+	}
+
+}
