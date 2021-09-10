adf855b1a6138ce2dd66922ffedefbf87e34257b
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-10 09:33:54 +0800

    Update arithmetic.java

--- arithmetic.java
+++ arithmetic.java
@@ -2,9 +2,11 @@ public class arithmetic {
  
  public static void main(String[] args) {  
  
+  //variable declaration
   int num1 = 5;
   int num2 = 10;
  
+  //displaying the output
   System.out.println(num1 + num2);
    
   System.out.println(num1 - num2);
