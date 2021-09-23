b51bf727a8583d3c8c47d8e2ac9b6dd7694e62c3
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-23 17:52:57 +0800

    Delete AccessModifier, Conditional & Switch/Access Modifier/Conditional directory

--- AccessModifier, Conditional & Switch/Access Modifier/Conditional/Short_hand.java
+++
@@ -1,8 +0,0 @@
-public class Short_hand {
-  public static void main(String[] args) {   
-    int time = 20;
-    String result;
-    result = (time < 18) ? "Good day." : "Good evening."; //using java short hand conditional statement 
-    System.out.println(result);
-  }
-}
--- AccessModifier, Conditional & Switch/Access Modifier/Conditional/if_con.java
+++
@@ -1,9 +0,0 @@
-public class if_con {
-  public static void main(String[] args) {
-    int x = 20;
-    int y = 18;
-    if (x > y) {
-      System.out.println("x is greater than y");
-    }  
-  }
-}
