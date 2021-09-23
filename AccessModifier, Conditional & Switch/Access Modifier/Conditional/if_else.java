2e2c7f0e538763605657245c82782eaebc425a8f
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-23 17:48:41 +0800

    Create if_else.java

---
+++ AccessModifier, Conditional & Switch/Access Modifier/Conditional/if_else.java
@@ -0,0 +1,10 @@
+public class if_else {
+  public static void main(String[] args) {
+    int time = 20;
+    if (time < 18) {
+      System.out.println("Good day.");
+    } else {
+      System.out.println("Good evening.");
+    }  
+  }
+}
